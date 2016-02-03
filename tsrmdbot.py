32c6c99de220f2d132a395f81ce17211fd18e9f0
David Soh <sotong0221@gmail.com>
2016-02-03 14:39:10 +0800

    auto repeat

--- tsrmdbot.py
+++ tsrmdbot.py
@@ -7,6 +7,7 @@ def handle(msg):
     print(msg)
     # Do your stuff here ...
     #postmsg=msg[]
+    bot.sendMessage(msg['chat']['id'], msg['chat']['first_name']+" "+msg['chat']['last_name']+" say: "+msg['text'])
 
 # Getting the token from command-line is better than embedding it in code,
 # because tokens are supposed to be kept secret.
