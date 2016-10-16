b8f3c87e636595e0d73a640c1e6395971410b414
sotongdj <sotong0221@gmail.com>
2016-10-16 22:14:43 +0800

    finishing prototype of emotional variation

--- tsrmdbot.py
+++ tsrmdbot.py
@@ -6,28 +6,55 @@ import externa
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
+    os.system("mkdir -p ./database/"+str(chat_id)+"/")
     moda=externa.check("mode",chat_id)
+
     if moda == "":
         externa.change("mode","none",chat_id)
-    if moda == "feel-analisi":
-        os.system("mkdir "+externa.path("analisi",str(chat_id)))
-        if content_type == 'text':
-            if "feel" in msg['text']:
-                fif=open(externa.path("analisi",str(chat_id))+"feeling.csv","a")
-                fif.write(str(chat_id)+",\""+msg['text']+"\"\n")
-                fif.close()
-                bot.sendMessage(chat_id, msg['text'])
-    elif moda == "none":
-        if content_type == 'text':
-            if msg['text']=="/help":
-                bot.sendMessage(chat_id, "Commands:\n/help\n    Show this document\n/start\n    Choose moda\n")
-            elif msg['text']=="/feel":
-                bot.sendMessage(chat_id,"Mode change from \""+moda+"\" to \"feel-analisi\"")
-            elif msg['text']=="/mode":
-                bot.sendMessage(chat_id,"Current mode is \""+moda+"\"")
-            else:
-                bot.sendMessage(chat_id, msg['text'])
 
+    if content_type == 'text':
+        if msg['text']=="/help":
+            helpmsg=externa.msg("help")
+            bot.sendMessage(chat_id, helpmsg)
+
+        elif msg['text']=="/start":
+            startmsg=externa.msg("start")
+            bot.sendMessage(chat_id,startmsg)
+
+        elif msg['text']=="/mode":
+            bot.sendMessage(chat_id,"Current mode is \""+moda+"\"")
+
+        elif msg['text']=="/feel":
+            externa.change("mode","analisi/feel",chat_id)
+            bot.sendMessage(chat_id,"Mode change from \""+moda+"\" to \"Analisi/Feel\"")
+
+        elif msg['text']=="/quit":
+            externa.change("mode","none",chat_id)
+            bot.sendMessage(chat_id,"Successfully quit mode from \""+moda+"\"")
+
+        elif moda == "analisi/feel":
+            level=0
+            limit=1
+            plus=2
+            for keyword in ["Feel","feel"]:
+                if keyword in msg['text']:
+                    level=level+plus
+            print("level="+str(level)+", limit="+str(limit))
+            if level > limit:
+                fif=open(externa.path("analisi/feel",str(chat_id))+"feeling.csv","a")
+                fif.write(str(chat_id)+","+str(msg['date'])+",\""+msg['text']+"\"\n")
+                fif.close()
+                bot.sendMessage(chat_id, "Recorded, Original message:\n\""+msg['text']+"\"")
+            elif level > 0:
+                bot.sendMessage(chat_id, "Recognized but lower the threshold, Original message:\n\""+msg['text']+"\"")
+            elif level == 0:
+                bot.sendMessage(chat_id, "Can't recognize, Original message:\n\""+msg['text']+"\"")
+
+        else:
+            bot.sendMessage(chat_id, "No action, Original message:\n\""+msg['text']+"\"")
+
+    else:
+        bot.sendMessage(chat_id, "No action, Original message:\n\""+msg['text']+"\"")
 TOKEN = sys.argv[1]  # get token from command-line
 
 bot = telepot.Bot(TOKEN)
