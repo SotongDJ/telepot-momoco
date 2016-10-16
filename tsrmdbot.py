5c65db4d68dc85f11f5cc50b96ac490549f0935d
sotongdj <sotong0221@gmail.com>
2016-10-16 20:19:26 +0800

    checking mode

--- tsrmdbot.py
+++ tsrmdbot.py
@@ -2,16 +2,18 @@ import sys
 import os
 import time
 import telepot
-import trbpath
-global moda,modaOld
+import externa
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
+    moda=externa.check("mode",chat_id)
+    if moda == "":
+        externa.change("mode","none",chat_id)
     if moda == "feel-analisi":
-        os.system("mkdir "+trbpath.path("analisi",str(chat_id)))
+        os.system("mkdir "+externa.path("analisi",str(chat_id)))
         if content_type == 'text':
             if "feel" in msg['text']:
-                fif=open(trbpath.path("analisi",str(chat_id))+"feeling.csv","a")
+                fif=open(externa.path("analisi",str(chat_id))+"feeling.csv","a")
                 fif.write(str(chat_id)+",\""+msg['text']+"\"\n")
                 fif.close()
                 bot.sendMessage(chat_id, msg['text'])
@@ -20,10 +22,9 @@ def handle(msg):
             if msg['text']=="/help":
                 bot.sendMessage(chat_id, "Commands:\n/help\n    Show this document\n/start\n    Choose moda\n")
             elif msg['text']=="/feel":
-                modaOld=moda
-                moda="feel-analisi"
-                bot.sendMessage(chat_id,"Mode change from \""+modaOld+"\" to \""+moda+"\"")
-                modaOld=""
+                bot.sendMessage(chat_id,"Mode change from \""+moda+"\" to \"feel-analisi\"")
+            elif msg['text']=="/mode":
+                bot.sendMessage(chat_id,"Current mode is \""+moda+"\"")
             else:
                 bot.sendMessage(chat_id, msg['text'])
 
@@ -31,9 +32,6 @@ TOKEN = sys.argv[1]  # get token from command-line
 
 bot = telepot.Bot(TOKEN)
 bot.message_loop(handle)
-#
-moda="none"
-modaOld=""
 #
 print ('Listening ...')
 
