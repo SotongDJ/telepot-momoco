e5fcc77b9cb1004836e1638c98fe4388382ec2c9
David Soh <sotong0221@gmail.com>
2017-04-10 00:45:08 +0800

    improve
    1, add time into data
    2, change all data to string, no more int
    3, modulize command respond (but still developing)
    4, modulize and tweak respond msg
    5, remove unuse code from guess.py

--- momocobot.py
+++ momocobot.py
@@ -1,33 +1,77 @@
-import sys, os, traceback, telepot, tool, auth, log
+import sys, os, traceback, telepot, time, tool, auth, log
 from telepot.delegate import per_chat_id, create_open, pave_event_space
 
 class User(telepot.helper.ChatHandler):
     def __init__(self, *args, **kwargs):
         super(User, self).__init__(*args, **kwargs)
-        self._tem = {"temp":"", "text":"", "numo":0,}
+        self._tem = {"temp":"", "text":"", "numo":"",}
         self._mem = {
             "mode":"",
             "namma":"",
             "klass":"",
             "shoop":"",
-            "emout":0,
+            "datte":"",
+            "price":"",
             "karen":"",
             "fromm":"",
             "toooo":"",
         }
     #
-    def _ask(self):
-        self.sender.sendMessage("""Welcome!
+    def _ask(self,text):
+        if "/start" in text:
+            self.sender.sendMessage("""Welcome!
 This is Money Money Come Chatbot.
 It can help you to trace your money flow more easily (Sure?)
 Reply /help to learn more~""")
+        elif "/help" in text:
+            self.sender.sendMessage("""This chatbot is under constructing...""")
 
-    #def _msg(self):
+    def _msg(self,msg,statu):
+        ma="Status:\n"+statu+"\nInput:\n"+msg["text"]+"\n-------------------------"
+        mb="\nPending: "+self._tem[self._tem["temp"]]
+        mc="\n/Product : "+self._mem["namma"]+"\n/Class : "+self._mem["klass"]+"\n/Seller : "+self._mem["shoop"]
+        md="\n/Date : "+self._mem["datte"]
+        me="\n/Price : "+self._mem["price"]
+        mf="\n/Currency : "+"\nSpent from: (what acc.)"+"\nTarget account: (If have)"# from self._setting
+        mg="\n-------------------------"+"\nTotal spent today:"
+        mh="\n-------------------------"+"\n/discard to discard"+"\n/add to add new account"+"\n/setting to change the setting"
+        return ma+mb+mc+md+me+mf+mg+mh
+
+    def _comme(self,text):
+        print("")
 
     def open(self, initial_msg, seed): # Welcome Region
         # self.sender.sendMessage('Guess my number')
         content_type, chat_type, chat_id = telepot.glance(initial_msg)
+        print("-------")
         print(initial_msg)
+        print(self._tem)
+        print(self._mem)
+        self._mem["mode"] = "inti"
+        if content_type != 'text':
+            self.sender.sendMessage("Status:\n    Received wrong message\nInput:\n    Undetactable content type\n")
+            self.close()
+            return
+        self._mem["datte"] = str(time.localtime(time.time())[0])+"-"+str(time.localtime(time.time())[1])+"-"+str(time.localtime(time.time())[2])
+        try:
+            numo = int(initial_msg["text"])
+            self._tem["numo"] = initial_msg["text"]
+            self._tem["temp"] = "numo"
+            self.sender.sendMessage(self._msg(initial_msg,"Creating new record"))
+        except ValueError:
+            if "/" in initial_msg["text"]:
+                self._comme(initial_msg["text"])
+                self.close()
+            else:
+                self._tem["nama"] = initial_msg["text"]
+                self._tem["temp"] = "nama"
+                self.sender.sendMessage(self._msg(initial_msg,"Creating new record"))
+        return True  # prevent on_message() from being called on the initial message
+
+    def on_chat_message(self, msg): # Each Msg
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        print("-------")
+        print(msg)
         print(self._tem)
         print(self._mem)
         self._mem["mode"] = "inti"
@@ -37,68 +81,18 @@ Reply /help to learn more~""")
             return
 
         try:
-            numo = int(initial_msg["text"])
-            self._tem["numo"] = numo
+            numo = int(msg["text"])
+            self._tem["numo"] = msg["text"]
             self._tem["temp"] = "numo"
-            self.sender.sendMessage(
-                "Status:\n    Creating new record\nInput:\n    "+
-                initial_msg["text"]+
-                "\n-------------------------\nPending:"+initial_msg["text"]+
-                "/Product Name:\n/Class :\n/Seller :\n/Price :\n/Currency :\n"+
-                ""+ # Currency from self._setting
-                "Spent from:\n"+
-                ""+ # Source from self._setting
-                "Destination: (If have)"+
-                "" # Deposit from self._setting
-            )
+            self.sender.sendMessage(self._msg(msg,"Creating on the way"))
         except ValueError:
-            if "/" in initial_msg["text"]:
-                if "/start" in initial_msg["text"]:
-                    self._ask()
-                    self.close()
+            if "/" in msg["text"]:
+                self._comme(msg["text"])
+                self.close()
             else:
-                self._tem["nama"] = initial_msg["text"]
+                self._tem["nama"] = msg["text"]
                 self._tem["temp"] = "nama"
-                self.sender.sendMessage(
-                    "Status:\n    Creating new record\nInput:\n    "+
-                    initial_msg["text"]+
-                    "\n-------------------------\nPending:\n    "+initial_msg["text"]+
-                    "/Product name:\n/Class :\n/Seller :\n/Price :\n/Currency :\n"+
-                    ""+ # Currency from self._setting
-                    "Spent from: (what acc.)\n"+
-                    ""+ # Source from self._setting
-                    "Target account: (If have)\n"+
-                    ""+
-                    "\n-------------------------\n"+
-                    ""+
-                    "\n-------------------------\n/discard to discard\n/add to add new account\n/setting" # Deposit from self._setting
-                )
-        return True  # prevent on_message() from being called on the initial message
-
-    def on_chat_message(self, msg): # Each Msg
-        content_type, chat_type, chat_id = telepot.glance(msg)
-        print("-------")
-        print(msg)
-        print(self._tem)
-        print(self._mem)
-        if content_type != 'text':
-            self.sender.sendMessage('Give me a number, please.')
-            return
-
-        try:
-           guess = int(msg['text'])
-        except ValueError:
-            self.sender.sendMessage('Give me a number, please.')
-            return
-
-        # check the guess against the answer ...
-        if guess != self._answer:
-            # give a descriptive hint
-            hint = self._hint(self._answer, guess)
-            self.sender.sendMessage(hint)
-        else:
-            self.sender.sendMessage('Correct!')
-            self.close()
+                self.sender.sendMessage(self._msg(msg,"Creating on the way"))
 
     def on__idle(self, event): # Timeout Region
         self.sender.sendMessage("Time's out")
