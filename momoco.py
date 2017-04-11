711ad348188a93e48e9204286804f5449c6c888e
David Soh <sotong0221@gmail.com>
2017-04-11 19:47:02 +0800

    restructure
    befor finish 2.0 restruction, start 2.1 restruction
    2.0 restruction:
    design card first
    construc data format
    complete script later
    
    2.1 restruction
    turn msg (with a lot if statement )into a single py that contain
    multiple module

--- momoco.py
+++ momoco.py
@@ -1,5 +1,174 @@
 import tool, json
 
+def chstr(a,b,c): # if a == "", return b; else return c
+    if a == "" :
+        return b
+    else:
+        return c
+
+def start(self):
+    final="""  Welcome
+----------------------------------------------
+This is Money Money Come Chatbot.
+It can help you to trace your money flow more
+easily (Sure? )
+
+Pls setup before using:
+    Account setup = /set_Account
+    Currency setup = /set_Currency
+----------------------------------------------
+    /help    /Setting
+
+This chatbot is under constructing...
+"""
+    self.sender.sendMessage(final)
+
+def msg(self,comme,msg,dicto):
+    final="Error: missing 'comme' "
+
+    elif comme == "help":
+        final="""  Help
+----------------------------------------------
+/start
+    Welcome Card
+/help
+    Command List Card
+/New
+    Creating  Card
+/List
+    Record Showing Card
+/Setting
+    Setting Card
+----------------------------------------------
+
+This chatbot is under constructing...
+"""
+    self.sender.sendMessage(final)
+def msg(self,comme,msg,dicto):
+    final="Error: missing 'comme' "
+
+    elif comme == "newStart":
+        final="""New Record
+----------------------------------------------
+Date: """+dicto["datte"]+"""
+Product: """+dicto["namma"]+"""
+Class: """+dicto["klass"]+"""
+Seller: """+dicto["shoop"]+"""
+Price: """+dicto["price"]+"""
+
+Currency: """+dicto["karen"]+"""
+    ( /change_Currency )
+
+Spent from which Account:
+"""+chstr(dicto["fromm"],"    ( /choose_Acc_From )",dicto["fromm"]+"\n    ( /choose_Acc_From )")+"""
+
+Transfer to which Account: (if have)
+"""+chstr(dicto["toooo"],"    ( /choose_Acc_To )",dicto["toooo"]+"\n    ( /choose_Acc_To )")+"""
+
+----------------------------------------------
+Keyword: """+dicto["text"]+"""
+    /set_as_Date (format: yyyy-mm-dd)
+    /set_as_Product /set_as_Class
+    /set_as_Seller /set_as_Price
+----------------------------------------------
+    /Discard    /Save    /List    /Setting
+
+This chatbot is under constructing...
+"""
+
+    self.sender.sendMessage(final)
+def msg(self,comme,msg,dicto):
+    final="Error: missing 'comme' "
+
+    elif comme == "newConti":
+        final="""Filling the blank
+----------------------------------------------
+Date: """+dicto["datte"]+"""
+Product: """+dicto["namma"]+"""
+Class: """+dicto["klass"]+"""
+Seller: """+dicto["shoop"]+"""
+Price: """+dicto["price"]+"""
+
+Currency: """+dicto["karen"]+"""
+    ( /change_Currency )
+
+Spent from which Account:
+"""+chstr(dicto["fromm"],"    ( /choose_Acc_From )",dicto["fromm"]+"\n    ( /choose_Acc_From )")+"""
+
+Transfer to which Account: (if have)
+"""+chstr(dicto["toooo"],"    ( /choose_Acc_To )",dicto["toooo"]+"\n    ( /choose_Acc_To )")+"""
+
+----------------------------------------------
+Keyword: """+dicto["text"]+"""
+    /set_as_Date (format: yyyy-mm-dd)
+    /set_as_Product /set_as_Class
+    /set_as_Seller /set_as_Price
+----------------------------------------------
+    /Discard    /Save    /List    /Setting
+
+This chatbot is under constructing...
+"""
+    self.sender.sendMessage(final)
+def msg(self,comme,msg,dicto):
+    final="Error: missing 'comme' "
+
+
+    elif comme == "newFinish":
+        final="""New Record Saved
+----------------------------------------------
+Date: """+dicto["datte"]+"""
+Product: """+dicto["namma"]+"""
+Class: """+dicto["klass"]+"""
+Seller: """+dicto["shoop"]+"""
+Price: """+dicto["price"]+"""
+
+Currency: """+dicto["karen"]+"""
+
+Spent from which Account:
+"""+dicto["fromm"]+"""
+
+Transfer to which Account: (if have)
+"""+dicto["toooo"]+"""
+
+----------------------------------------------
+    /Edit    /List    /Setting
+
+This chatbot is under constructing...
+"""
+    self.sender.sendMessage(final)
+def msg(self,comme,msg,dicto):
+    final="Error: missing 'comme' "
+
+    elif comme == "listFilter":
+        final="""Filtering Card
+----------------------------------------------
+Date range:"""+dicto["datte"]+"""
+/Today /This_Week /This_Month /This_Year
+
+----------------------------------------------
+    /Discard    /Summit    /Setting
+
+This chatbot is under constructing...
+"""
+    self.sender.sendMessage(final)
+def msg(self,comme,msg,dicto):
+    final="Error: missing 'comme' "
+
+    elif comme == "home": # will replace by Start Card after finished Account Card and Currency Card
+        final="""Home Card (Temporary)
+----------------------------------------------
+Welcome Home !
+Keyword : """+"""
+----------------------------------------------
+    /Discard    /Summit    /Setting
+
+This chatbot is under constructing...
+"""
+    self.sender.sendMessage(final)
+
+# Above : Recvontruction
+# Below : Old
+
 def check(file_name):
     a=open(file_name,"r")
     b=json.load(a)
