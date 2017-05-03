86e1d0eec279e6956873a7b1aee6bc68dcf7bd43
David Soh <sotong0221@gmail.com>
2017-05-04 02:30:31 +0800

    adjust ckpy

--- trmbot.py
+++ trmbot.py
@@ -35,7 +35,8 @@ class User(telepot.helper.ChatHandler):
         elif "/ckpy" in text:
             fille = json.load(open("database/opt/bot.json",'r'))
             self.sender.sendMessage(pprint.pformat(fille))
-            self.sender.sendMessage(pprint.pformat(open("database/opt/bot.pid").read()))
+            subprocess.call(['pgrep', 'python'], stdout=open('database/opt/bot.pid', 'w'))
+            self.sender.sendMessage(pprint.pformat(''.join(open("database/opt/bot.pid").read().splitlines())))
 
         elif len(self._mod) == 0:
             self.sender.sendMessage("I don't know that you said")
