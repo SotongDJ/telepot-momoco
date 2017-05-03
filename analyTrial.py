a0ce2bafb74fe2dd6b991e62cc4c702165396388
David Soh <sotong0221@gmail.com>
2017-05-04 01:51:44 +0800

    adjust calculation

--- analyTrial.py
+++ analyTrial.py
@@ -1,4 +1,5 @@
 import mmcdb, pprint
+import halfu
 from libmsg import listMsg
 # print(trial3.main(chat_id,20,'2017-04',['shoop',?],'namma'),'TWD')
 # print(trial3.main(chat_id,20,'2017-04',['klass',?],'shoop','TWD'))
@@ -26,8 +27,8 @@ def main(usrid,setto):
         for uid in libra['key']['datte'][tim]:
             if libra['raw'][uid]['karen'] == karen:
                 if libra['raw'][uid][conda] == conde:
-                    tat=libra['raw'][uid]['datte']+'　'+libra['raw'][uid]['namma']+'　'+libra['raw'][uid]['shoop']+'\n　　　　　　　　'
-                    tat=tat+'　'+libra['raw'][uid]['klass']+'　'+libra['raw'][uid]['fromm']+'　'+karen+' '+libra['raw'][uid]['price']
+                    tat=libra['raw'][uid]['datte']+'　'+libra['raw'][uid]['fromm']+'　'+karen+' '+libra['raw'][uid]['price']
+                    tat=tat+'\n　　　　　'+libra['raw'][uid]['klass']+'　'+libra['raw'][uid]['namma']+'　'+libra['raw'][uid]['shoop']
                     uuid.append(tat)
                     try:
                         gas[libra['raw'][uid][targe]] = gas[libra['raw'][uid][targe]] + int(libra['raw'][uid]['price'])
@@ -36,54 +37,66 @@ def main(usrid,setto):
     som = sum(list(gas.values()))
     for nana in list(gas):
         lata = round((gas[nana]/som)*(lim*lim))
+        pprint.pprint([lata,'=gas[',nana,']',gas[nana],'/',som,')*(',lim*lim,')'])
         gos[nana]=lata
         loto = str(round((gas[nana]/som)*(100),2))
         gus[nana]=loto
         try:
-            gis[lata].append(nana)
+            gis[gas[nana]].append(nana)
         except KeyError:
-            gis[lata]=[nana]
+            gis[gas[nana]]=[nana]
     nanga = sorted( gis , reverse = True)
     #tank = 'abcdefigmhnjrkspuqvtwyxz'
     pri = []
     #pro = 0
+    pprint.pprint([nanga,gis])
     for nume in nanga:
         for itei in gis[nume]:
+            nota = gos[itei]
+            try:
+                itei[0].encode('latin-1')
+                miro=halfu.fullen(itei[0])
+            except UnicodeEncodeError:
+                miro=itei[0]
             try:
                 rok = len(pri[-1])
             except IndexError:
                 pri = ['']
                 rok = 0
-            if nume + rok >lim:
+            pprint.pprint([itei,nota,rok,lim])
+            if nota + rok >lim:
                 if rok < lim:
                     #pri[-1]=pri[-1]+tank[pro]*(lim-rok)
-                    pri[-1]=pri[-1]+itei[0]*(lim-rok)
+                    pri[-1]=pri[-1]+miro*(lim-rok)
                     #print(pri[-1])
-                    nume = nume + rok - lim
-                    if nume > lim:
-                        tik = int(nume/lim)
-                        tok = nume % lim
+                    nota = nota + rok - lim
+                    if nota > lim:
+                        tik = int(nota/lim)
+                        tok = nota % lim
                         for n in range(0,tik):
                     #        print(tank[pro]*10)
                     #        pri.append(tank[pro]*lim)
-                            pri.append(itei[0]*lim)
+                            pri.append(miro*lim)
                     #    pri.append(tank[pro]*tok)
-                        pri.append(itei[0]*tok)
+                        pri.append(miro*tok)
                         #pro=pro+1
-                    elif nume <= lim:
-                    #    pri.append(tank[pro]*nume)itei[0]
-                        pri.append(itei[0]*nume)
+                        pprint.pprint([itei,nota,rok,lim])
+                    elif nota <= lim:
+                    #    pri.append(tank[pro]*nume)miro
+                        pri.append(miro*nota)
                         #pro=pro+1
-            elif nume + rok <= lim:
+                        pprint.pprint([itei,nota,rok,lim])
+            elif nota + rok <= lim:
                 #pri[-1]=pri[-1]+tank[pro]*nume
-                pri[-1]=pri[-1]+itei[0]*nume
+                pri[-1]=pri[-1]+miro*nota
                 #print(pri[-1])
                 #pro=pro+1
+                pprint.pprint([itei,nota,rok,lim])
     des=""
     for m in nanga:
         for n in gis[m]:
             nana = n
-            des=des+'　'+nana[0]+'　'+nana+'\n　　　'+karen+' '+str(gas[nana])+' ('+gus[nana]+'%)\n'
+            des=des+'　'+nana[0]+'　'+nana+'\n　　　'+karen+' '+str(gas[nana])+' ('+gus[nana]+'%, '+str(gos[nana])+')\n'
     final = [
         'Analytics Card',
         'Record:\n——————————\n  '+'\n  '.join(uuid)+'\n',
