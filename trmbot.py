e176907d91c3431f05d5383c10a07e73ded36811
David Soh <sotong0221@gmail.com>
2017-05-04 17:22:57 +0800

    remove meaningless part

--- trmbot.py
+++ trmbot.py
@@ -51,13 +51,6 @@ class User(telepot.helper.ChatHandler):
             print('/update')
             tool.ckpath('database/opt/','daemon.log')
             subprocess.Popen(['python3', 'daemon.py'],stdout=open('database/opt/daemon.log','w'))
-            time.sleep(1)
-            fille = json.load(open("database/opt/bot.json",'r'))
-            self.sender.sendMessage(pprint.pformat(fille))
-            subprocess.call(['pgrep','-l', 'python'], stdout=open('database/opt/bot.pid', 'w'))
-            self.sender.sendMessage('\n'.join(open("database/opt/bot.pid").read().splitlines()))
-            self.sender.sendMessage('Send /ckpy to refresh status above')
-            self.close()
         elif "/ckpy" in text:
             fille = json.load(open("database/opt/bot.json",'r'))
             self.sender.sendMessage(pprint.pformat(fille))
