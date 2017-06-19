523806ba8a62d926acb1f1cc9b1269fe90081e4f
David Soh <sotong0221@gmail.com>
2017-06-19 10:42:41 +0800

    split msgCreo from msgOuto

--- libmsgMmc/msgOuto.py
+++ libmsgMmc/msgOuto.py
@@ -31,42 +31,6 @@ Notes:
 """
     return final
 
-""" self.sender.sendMessage(mmcmsg.outoKeywo(keywo)) """
-def keyword(keywo):
-    final="""Filling the blank
-——————————
-Keyword:
-　"""+keywo+"""
-
-　/set_as_Date
-(Format of Date: yyyy-mm-dd)
-　/set_as_Item　/set_as_Category
-
-　/set_as_Seller　/set_as_Notes
-——————————
-　/set_as_Price　/set_as_Account
-
-　/set_as_Currency_Source
-——————————
-　/Discard　/whats_now
-"""
-    return final
-
-""" self.sender.sendMessage(self._recom[1],self._keywo) """
-def recom(txt,keywo):
-    final="""Recommend Card
-——————————
-Last Keyword: (Typing)
-　　"""+keywo+"""
-Recommend List:
-"""+txt+"""
-——————————
-　/Discard　/Save　/setting
-Remind:
-　Choose above or type another keyword
-"""
-    return final
-
 """ self.sender.sendMessage(mmcmsg.outoConti(self._temra)) """
 def finis(dicto):
     final="""New #Expense Record #Saved
@@ -82,17 +46,5 @@ Notes:
 """+dicto["desci"]+"""
 ——————————
 　/Edit　/list　/setting
-"""
-    return final
-
-""" self.sender.sendMessage(mmcmsg.outoDiscard()) """
-def discard():
-    final="""¡ Discard !
-——————————
-
-　Closed Creating Card
-
-——————————
-　/setting　/help
 """
     return final
