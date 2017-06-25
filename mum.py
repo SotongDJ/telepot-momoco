40c0ecccbdcca7727934b8a82b3d75687b4af719
David Soh <sotong0221@gmail.com>
2017-06-25 11:31:48 +0800

    Finally finish trial

--- mum.py
+++ mum.py
@@ -8,7 +8,7 @@ from son import LoginScreen
 class MyApp(App):
 
     def build(self):
-        return LoginScreen()
+        return LoginScreen(uOtext='None',pOtext='None')
 
 
 if __name__ == '__main__':
--- son.py
+++ son.py
@@ -3,6 +3,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+from kivy.properties import ObjectProperty, StringProperty
 
 class LoginScreen(GridLayout):
 
@@ -11,37 +12,29 @@ class LoginScreen(GridLayout):
     usnanIn = ObjectProperty()
     paswdIn = ObjectProperty()
     resut = ObjectProperty()
-    usnanOut = StringProperty()
-    paswdOut = StringProperty()
+    usnanOut = ObjectProperty()
+    paswdOut = ObjectProperty()
 
-    def sumited(self, obj):
-        self.resut.usnanOut = self.usnanIn.text
-        self.resut.paswdOut = self.paswdIn.text
+    usnanText = StringProperty()
+    paswdText = StringProperty()
+    sumitText = StringProperty()
+    uOtext = StringProperty()
+    pOtext = StringProperty()
+
+    def sendinu(self,tast):
+        self.uOtext = tast
+        print(self.uOtext + tast)
+
+    def sendinp(self,tast):
+        self.pOtext = tast
+        print(self.pOtext + tast)
+
+    def sumited(self,taa,tao):
+        self.uOtext = taa
+        self.pOtext = tao
 
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
-        self.rows = 4
-        self.usnan = GridLayout(cols=2)
-
-        self.usnanIn = TextInput(multiline=False)
-        self.usnan.add_widget(Label(text='名稱'))
-        self.usnan.add_widget(self.usnanIn)
-        self.add_widget(self.usnan)
-
-        self.paswd = GridLayout(cols=2)
-        self.paswdIn = TextInput(password=True, multiline=False)
-        self.paswd.add_widget(Label(text='密碼'))
-        self.paswd.add_widget(self.paswdIn)
-        self.add_widget(self.paswd)
-
-        self.button = Button(text='提交')
-        self.button.bind(on_release=self.sumited)
-        self.add_widget(self.button)
-
-        self.usnanOut = ''
-        self.paswdOut = ''
-
-        self.result = GridLayout(cols=2)
-        self.result.add_widget(Label(text='名稱：'+self.usnanOut))
-        self.result.add_widget(Label(text='密碼：'+self.paswdOut))
-        self.add_widget(self.result)
+        self.usnanText = '名稱'
+        self.paswdText = '密碼'
+        self.sumitText = '提交'
