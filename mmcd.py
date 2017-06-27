8db01a54f2995a869154cc7b0648050f6db86b10
David Soh <sotong0221@gmail.com>
2017-06-27 08:06:39 +0800

    remove unnecessary files

--- mmcd.py
+++
@@ -1,5 +0,0 @@
-import datran,time
-print('python3 mmcd.py')
-print('    datran.mmcd()')
-datran.mmcd()
-print('mmcd.py closed')
--- mmckill.py
+++
@@ -1,5 +0,0 @@
-import datran,time
-print('python3 kmmc.py')
-print('    datran.kmmc()')
-datran.kmmc()
-print('kmmc.py closed')
