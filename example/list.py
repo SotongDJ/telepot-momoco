4160eb90adc9d28ead3f172287e0e9328175c6bb
David Soh <sotong0221@gmail.com>
2017-06-28 11:12:16 +0800

    Finish Testing. Result: Success.

--- example/list.py
+++ example/list.py
@@ -4,73 +4,99 @@ os.environ['KIVY_METRICS_DENSITY'] = '2'
 
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
-from kivy.uix.scrollview import ScrollView
-from kivy.core.window import Window
 from kivy.properties import ObjectProperty, StringProperty
-
-class Buto(Button):
-    inTa = StringProperty()
-
-    def pus(self):
-        print(self.inTa)
-
-class Selection(AnchorLayout):
-    usrdir = StringProperty()
-    def login(self,tar):
-        tar.clear_widgets()
-        tar.add_widget(LoginScreen(usrdir=usrdir))
+import pprint
 
 
-class LoginScreen(GridLayout):
+class CreateCard(GridLayout):
 
-    usnan = ObjectProperty()
-    paswd = ObjectProperty()
-    resut = ObjectProperty()
-    hinti = ObjectProperty()
+    datte = ObjectProperty()
+    namma = ObjectProperty()
+    klass = ObjectProperty()
+    shoop = ObjectProperty()
+    fromm = ObjectProperty()
+    price = ObjectProperty()
+    karen = ObjectProperty()
+    toooo = ObjectProperty()
+    tpric = ObjectProperty()
+    tkare = ObjectProperty()
+    desci = ObjectProperty()
 
-    usnanTitle = StringProperty()
-    paswdTitle = StringProperty()
+    datteTitle = StringProperty()
+    nammaTitle = StringProperty()
+    klassTitle = StringProperty()
+    shoopTitle = StringProperty()
+    frommTitle = StringProperty()
+    priceTitle = StringProperty()
+    karenTitle = StringProperty()
+    tooooTitle = StringProperty()
+    tpricTitle = StringProperty()
+    tkareTitle = StringProperty()
+    desciTitle = StringProperty()
 
-    hintiText = StringProperty()
+    datteText = StringProperty()
+    nammaText = StringProperty()
+    klassText = StringProperty()
+    shoopText = StringProperty()
+    frommText = StringProperty()
+    priceText = StringProperty()
+    karenText = StringProperty()
+    tooooText = StringProperty()
+    tpricText = StringProperty()
+    tkareText = StringProperty()
+    desciText = StringProperty()
+
+    storeText = StringProperty()
     sumitText = StringProperty()
     usrdir = StringProperty()
 
-    def sumited(self,tar,taa,tao):
-        wd = "Name:"+taa+", Password:"+tao
-        laba = Buto(text=wd,inTa=tao)
-        tar.add_widget(laba)
+    def stored(self,fro,too,tar,tan):
+        tar.text = fro
+        self.temra.update({ tan : fro })
 
-    def close(self):
-        App.get_running_app().stop()
+    def sumited(self):
+        pprint.pprint(self.temra)
 
     def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
-        self.usnanTitle = 'Name'
-        self.paswdTitle = 'Password'
+        super(CreateCard, self).__init__(**kwargs)
+        self.datteTitle = "Date: "
+        self.nammaTitle = "Name: "
+        self.klassTitle = "Class: "
+        self.shoopTitle = "Seller: "
+        self.frommTitle = "From: "
+        self.karenTitle = "Currency: "
+        self.priceTitle = "Price: "
+        self.tooooTitle = "To: "
+        self.tkareTitle = "Currency: "
+        self.tpricTitle = "Price: "
+        self.desciTitle = "Notes: "
+
+        self.storeText = 'Store -->'
         self.sumitText = 'Sumit'
 
-class Choose(GridLayout):
-    usrdir = StringProperty()
+        self.temra={
+            'datte' : self.datteText,
+            'namma' : self.nammaText,
+            'klass' : self.klassText,
+            'shoop' : self.shoopText,
+            'fromm' : self.frommText,
+            'price' : self.priceText,
+            'karen' : self.karenText,
+            'toooo' : self.tooooText,
+            'tpric' : self.tpricText,
+            'tkare' : self.tkareText,
+            'desci' : self.desciText,
+        }
 
-    def returan(self):
-        LasApp.hiwi.clear_widgets()
-        LasApp.hiwi.add_widget(Selection(usrdir=usrdir))
-
-    def __init__(self, **kwargs):
-        super(Choose, self).__init__(**kwargs)
-        LasApp.hiwi.add_widget(Selection(usrdir=usrdir))
-
-class LasApp(App):
-    hiwi = ObjectProperty()
+class DosApp(App):
 
     def build(self):
-        return Choose(usrdir=usrdir)
+        return CreateCard(usrdir=usrdir)
 
 
 if __name__ == '__main__':
-    usrdir = LasApp().user_data_dir
-    LasApp().run()
+    usrdir = DosApp().user_data_dir
+    DosApp().run()
