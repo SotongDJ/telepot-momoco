492b751e0984eb9b09da206313f44a37cb2b1306
David Soh <sotong0221@gmail.com>
2017-06-28 21:07:19 +0800

    add hint

--- modSearch.py
+++ modSearch.py
@@ -1,14 +1,19 @@
 import modDatabase, modVariables
 
 def sachi(usrdir,dicto):
+    print('modSearch.sachi: '+usrdir)
     libra = modDatabase.opendb(usrdir)
     rawdb = libra.get('raw',{})
     keydb = libra.get('key',{})
 
     keywo = dicto.get('keywo','')
+    print('keywo: '+keywo)
     dtempo = dicto.get('dtempo','')
+    print('dtempo: '+dtempo)
     utempo = dicto.get('utempo','')
+    print('utempo: '+utempo)
     cokas = dicto.get('cokas','')
+    print('cokas: '+cokas)
 
     tiset = modDatabase.timra(usrdir, dtempo=dtempo, utempo=utempo)
 
@@ -20,7 +25,7 @@ def sachi(usrdir,dicto):
     ralib = {}
     keyra = {}
     for keyo in kaset:
-        for valo in keydb.get(keyo).keys():
+        for valo in keydb.get(keyo,{}).keys():
             vakey = ralib.get(valo,[])
             nonom = len(vakey)
             vaset = keydb.get(keyo).get(valo)
@@ -41,25 +46,3 @@ def sachi(usrdir,dicto):
             keyto.update({ valo : keyra.get(valo,[]) })
 
     return { 'kelib': kelib , 'keyto': keyto }
-
-def listSachi(usrdir,libto,lingua='enMY'):
-    libra = modDatabase.opendb(usrdir)
-    rawdb = libra.get('raw',{})
-
-    kelib = libto.get('kelib',{})
-    keyto = libto.get('keyto',{})
-
-    transle = modVariables.keywo('transle',lingua=lingua)
-    resut = ''
-    for keyo in kelib.keys():
-        resut = resut + keyo+ ' (' + ', '.join([transle.get(x,'') for x in keyto.get(keyo,[]) ])+ ') ' + ': \n'
-        for uuid in set(kelib.get(keyo,[])):
-            resut = resut + '　/uuid_'+uuid+'\n　　'
-            resut = resut + rawdb.get(uuid,{}).get('datte','')+'　'
-            resut = resut + rawdb.get(uuid,{}).get('namma','')+'　'
-            resut = resut + rawdb.get(uuid,{}).get('klass','')+'　'
-            resut = resut + rawdb.get(uuid,{}).get('karen','')+' '
-            resut = resut + rawdb.get(uuid,{}).get('price','')+'\n'
-        resut = resut + '\n'
-
-    return resut
