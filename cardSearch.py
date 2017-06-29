4a69ec4620f9c4dcc5b5d8c994405bce49d61a7d
David Soh <sotong0221@gmail.com>
2017-06-29 17:45:07 +0800

    finish search func. of searching card. start develop result listing func.

--- cardSearch.py
+++ cardSearch.py
@@ -1,50 +1,35 @@
+# -*- coding: utf-8 -*-
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.properties import ObjectProperty, StringProperty
-import pprint
-import modDatabase
+import pprint, json
+import modSearch
 import tool
+
+class Labo(Label):
+    pass
 
 class SearchCard(GridLayout):
 
-    datte = ObjectProperty()
-    namma = ObjectProperty()
-    klass = ObjectProperty()
-    shoop = ObjectProperty()
-    fromm = ObjectProperty()
-    price = ObjectProperty()
-    karen = ObjectProperty()
-    toooo = ObjectProperty()
-    tpric = ObjectProperty()
-    tkare = ObjectProperty()
-    desci = ObjectProperty()
+    dtempo = ObjectProperty()
+    utempo = ObjectProperty()
+    cokas = ObjectProperty()
+    keywo = ObjectProperty()
 
-    datteTitle = StringProperty()
-    nammaTitle = StringProperty()
-    klassTitle = StringProperty()
-    shoopTitle = StringProperty()
-    frommTitle = StringProperty()
-    priceTitle = StringProperty()
-    karenTitle = StringProperty()
-    tooooTitle = StringProperty()
-    tpricTitle = StringProperty()
-    tkareTitle = StringProperty()
-    desciTitle = StringProperty()
+    tempor = ObjectProperty()
 
-    datteText = StringProperty()
-    nammaText = StringProperty()
-    klassText = StringProperty()
-    shoopText = StringProperty()
-    frommText = StringProperty()
-    priceText = StringProperty()
-    karenText = StringProperty()
-    tooooText = StringProperty()
-    tpricText = StringProperty()
-    tkareText = StringProperty()
-    desciText = StringProperty()
+    dtempoTitle = StringProperty()
+    utempoTitle = StringProperty()
+    cokasTitle = StringProperty()
+    keywoTitle = StringProperty()
+
+    dtempoText = StringProperty()
+    utempoText = StringProperty()
+    cokasText = StringProperty()
+    keywoText = StringProperty()
 
     storeText = StringProperty()
     sumitText = StringProperty()
@@ -53,48 +38,35 @@ class SearchCard(GridLayout):
     def stored(self,fro,too,tar,tan):
         too = fro
         tar.text = too
-        self.temra.update({ tan : too })
+        self.dicto.update({ tan : too })
 
-    def sumited(self):
-        pprint.pprint(self.temra)
-        modDatabase.addRaw(usrdir=self.usrdir,temra=self.temra)
+    def sumited(self,tar):
+        self.resut = modSearch.sachi(usrdir=self.usrdir,dicto=self.dicto)
+        tar.add_widget(Labo(text=pprint.pformat(self.resut,compact=True)))
 
     def __init__(self, **kwargs):
-        super(CreateCard, self).__init__(**kwargs)
-        self.datteTitle = "Date: "
-        self.nammaTitle = "Name: "
-        self.klassTitle = "Class: "
-        self.shoopTitle = "Seller: "
-        self.frommTitle = "From: "
-        self.karenTitle = "Currency: "
-        self.priceTitle = "Price: "
-        self.tooooTitle = "To: "
-        self.tkareTitle = "Currency: "
-        self.tpricTitle = "Price: "
-        self.desciTitle = "Notes: "
+        super(SearchCard, self).__init__(**kwargs)
+        self.dtempoTitle = "Start: "
+        self.utempoTitle = "End: "
+        self.cokasTitle = "Class: "
+        self.keywoTitle = "Keywo: "
 
         self.storeText = 'Store -->'
         self.sumitText = 'Sumit'
 
-        self.temra={
-            'datte' : self.datteText,
-            'namma' : self.nammaText,
-            'klass' : self.klassText,
-            'shoop' : self.shoopText,
-            'fromm' : self.frommText,
-            'price' : self.priceText,
-            'karen' : self.karenText,
-            'toooo' : self.tooooText,
-            'tpric' : self.tpricText,
-            'tkare' : self.tkareText,
-            'desci' : self.desciText,
+        self.dicto={
+            'dtempo' : self.dtempoText,
+            'utempo' : self.utempoText,
+            'cokas' : self.cokasText,
+            'keywo' : self.keywoText,
         }
 
 class SearchingApp(App):
 
     def build(self):
-        datteText = tool.date(modde=1)
-        return SearchCard(datteText=datteText,usrdir=usrdir)
+        dtempoText = tool.date(modde=6)
+        utempoText = tool.date(modde=6)
+        return SearchCard(dtempoText=dtempoText,utempoText=utempoText,usrdir=usrdir)
 
 if __name__ == '__main__':
     usrdir = SearchingApp().user_data_dir
