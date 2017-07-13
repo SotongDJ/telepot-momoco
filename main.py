0505ac2d03b79ec9a69026355e5e26d1575e2323
David Soh <sotong0221@gmail.com>
2017-07-13 16:41:24 +0800

    starting modifing main.py

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
-import os
-os.environ['KIVY_DPI'] = '320'
-os.environ['KIVY_METRICS_DENSITY'] = '2'
-
 from kivy.app import App
-from dbMgr import ListData
-from cardCreating import CreateCard
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.properties import ObjectProperty, StringProperty
 
-class CreatingApp(App):
+from kivy.lang import Builder
+Builder.load_file('cakvSearching.kv')
+
+import pprint
+import modSearch, modDatabase, modVariables
+import tool
+
+class MainCard(GridLayout):
+    def __init__(self, **kwargs):
+        super(MainApp, self).__init__(**kwargs)
+        self.prese = {}
+        self.prese.update({ 'setti' : modDatabase.openSetting(usrdir) })
+        self.prese.update({ 'temra' : modVariables.keywo('temra') })
+        self.prese.update({ 'stati' : modVariables.keywo('statics') })
+        self.prese.update({ 'listi' : modVariables.keywo('listi') })
+        self.prese.update({ 'karatio' : modDatabase.openKaratio(usrdir) })
+
+class MainApp(App):
 
     def build(self):
-        return CreateCard(usrdir=usrdir)
-
+        return MainCard()
 
 if __name__ == '__main__':
-    usrdir = CreatingApp().user_data_dir
-    CreatingApp().run()
+    usrdir = MainApp().user_data_dir
+    MainApp().run()
