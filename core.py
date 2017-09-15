1a9809172d45e41c9aa3e91aff03f12f72210ceb
David Soh <sotong0221@gmail.com>
2017-09-16 06:55:53 +0800

    add core.py

---
+++ core.py
@@ -0,0 +1,9 @@
+"""
+This is the main part of Momocobot to handle income msg
+and reply required msg back to user.
+
+The codes of this file mainly lineage from comme() in momocobot.py.
+"""
+
+def handle():
+    print('Under Developing')
