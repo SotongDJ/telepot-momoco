832fd9bcd5e09b1437ac40147cebe31de982b051
David Soh <sotong0221@gmail.com>
2017-09-16 20:23:04 +0800

    add excute and add description to both function

--- core.py
+++ core.py
@@ -6,5 +6,20 @@ and reply required msg back to user.
 The codes of this file mainly lineage from comme() in momocobot.py.
 """
 
-def handle():
-    print('Under Developing')
+def hande(msg={},arg={}):
+    """ handle received msg
+    pass command to excute() for futher action (which is more complex)
+    change cache info that store in memory (arg)
+    and reply back to user
+    """
+    mesut=[]
+    return {'mesut':mesut,'arg':arg}
+
+def excut(msg={},arg={}):
+    """ process  msg
+    grab command from hande()
+    assign task to other mod
+    and reply result back to hande()
+    """
+    resut=[]
+    return {'resut':resut,'arg':arg}
