1298724ce0b6eb782d078c1234b07dfc3cc628f0
David Soh <sotong0221@gmail.com>
2017-09-16 22:35:00 +0800

    rewrite open() to make it suitable to new framework

--- main.py
+++ main.py
@@ -45,27 +45,40 @@ class User(telepot.helper.ChatHandler):
 
     def open(self, initial_msg, seed): # Welcome Region
         content_type, chat_type, chat_id = telepot.glance(initial_msg)
-        self.printbug("Intitial",chat_id)
-        mmctool.printbug("inti_msg",initial_msg,chat_id)
-        self._mod = ['']
-        self._setting = mmcdb.upgradeSetting(self._setting,chat_id)
-        self._karatio = mmcdb.openKaratio()
-        self._rawdb = mmcdb.opendb(chat_id)['raw']
-        self._keydb = mmcdb.opendb(chat_id)['key']
-        self._vez=0
-        open(tool.path('log/mmcbot',usrid=auth.id())+tool.date(modde=1)+'.c','a').write('\n')
+        usrdir = 'database/usr/'+str(chat_id)
+        lingua = modDatabase.openSetting(usrdir=usrdir).get('lingua','enMY')
+        self.arg.update({
+            'usrdir' : usrdir,
+            'lingua' : lingua,
+            'catid' : chat_id,
+            'catyp' : chat_type,
+            'cotyp' : content_type,
+            'primo' : [''],
+            'submo' : '',
+            'setti' : modDatabase.openSetting(usrdir=usrdir),
+            'karat' : modDatabase.openKaratio(usrdir=usrdir),
+            'rawdb' : modDatabase.opendb(usrdir=usrdir).get('raw',{}),
+            'keydb' : modDatabase.opendb(usrdir=usrdir).get('key',{}),
+            'veces' : 0,
+        })
 
         if content_type != 'text':
-            self.sending(msgMain.error(), modda = 1)
+            self.sending(mesag=msgMain(lingua=lingua,tasta='error'))
             self.close()
             return
 
-        if "/" in initial_msg["text"]:
-            self.comme(initial_msg)
-        else:
-            if "/" not in initial_msg["text"]:
-                self._keywo = initial_msg["text"].replace(" ","_")
-            self.sending(msgMain.home(self._keywo))
+        if "/" in initial_msg['text']:
+            resul = hande(msg=initial_msg, arg=self.arg)
+            mesut = resul.get('mesut',[])
+            arg = self.arg
+            self.arg = resul.get('arg',arg)
+            cos = resul.get('cos',0)
+            if cos == 1:
+                self.close()
+        elif "/" not in initial_msg["text"]:
+            keywo = initial_msg["text"].replace(" ","_")
+            self.arg.update({ 'keywo' : keywo })
+            self.sending(mesag=msgMain(lingua=lingua,tasta='home',keyse={'keywo':keywo}))
 
         return True  # prevent on_message() from being called on the initial message
 
@@ -101,8 +114,9 @@ class User(telepot.helper.ChatHandler):
             self.sending(mesag=msgMain(lingua=lingua,tasta='home',keyse={'keywo':keywo}))
 
     def on__idle(self, event): # Timeout Region
-        lingua = self._setting['lingua']
-        self.sending(msgMain.timesout()+mainShort.woood(lingua,'cof') , modda = 1 )
+        usrdir = 'database/usr/'+str(self.arg.get('catid','admin'))
+        lingua = modDatabase.openSetting(usrdir=usrdir).get('lingua','enMY')
+        self.sending(mesag=msgMain(lingua=lingua,tasta='timesout')+mainShort.woood(lingua=lingua,tasta='cof'))
         self.close()
 
 key=json.load(open("database/key","r"))
