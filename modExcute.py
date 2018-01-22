ac351ce392d9d02122688137b16c916221268a63
David Soh <sotong0221@gmail.com>
2018-01-23 01:21:02 +0800

    fix "len(fikase) != len()" and "no answer = no max()"

--- modExcute.py
+++ modExcute.py
@@ -64,7 +64,7 @@ class Excut:
         kasdik = {} # {class : [keyword]}
         udilis = [] # [uuid]
         udidik = {} # {uuid : score }
-        secodi = {} # {score : uuid}
+        secodi = {0:[0]} # {score : uuid}
 
         for keyto in keywolista.keys():
             if keyto in self.argo.keywo:
@@ -97,46 +97,57 @@ class Excut:
         kasse = ''
         keywo = ''
         finudi = secodi.get(max(sorted(list(secodi.keys()))))
-        fikali = sorted(list(kasdik.keys()))
-        # pprint.pprint(fikeli)
-        for kasse in fikali:
-            if fikasi == "":
-                fikasi = kasse
-            else:
-                fikasi = fikasi + '@' + kasse
-        # print("fikasi:"+fikasi)
-        for kewoli in kasdik.values():
-            fikeli.extend(kewoli)
-        nummo = 0
-        for uuid in finudi:
-            metasi = ''
+        if finudi == [0]:
+            return {}
+        else:
+            fikali = sorted(list(kasdik.keys()))
+            # pprint.pprint(fikeli)
             for kasse in fikali:
-                keyta = rawdb.get(uuid).get(kasse)
-                # print("keyta:"+keyta)
-                if keyta in fikeli:
+                if fikasi == "":
+                    fikasi = kasse
+                else:
+                    fikasi = fikasi + '@' + kasse
+            # print("fikasi:"+fikasi)
+            for kewoli in kasdik.values():
+                fikeli.extend(kewoli)
+            nummo = 0
+            for uuid in finudi:
+                metasi = ''
+                for kasse in fikali:
+                    keyta = rawdb.get(uuid).get(kasse)
+                    # print("keyta:"+keyta)
+                    if keyta not in fikeli:
+                        keyta = ''
                     if metasi == '':
                         metasi = keyta
                     else:
                         metasi = metasi + '@' + keyta
-            nummo = fudidi.get(metasi,0)
-            nummo = nummo + 1
-            fudidi.update({ metasi : nummo })
-        # pprint.pprint(fudidi)
+                nummo = fudidi.get(metasi,0)
+                nummo = nummo + 1
+                fudidi.update({ metasi : nummo })
+            # pprint.pprint(fudidi)
 
-        resudi = {} # { score : last keywo-keywo}
-        for metakesi in fudidi.keys():
-            sekor = fudidi.get(metakesi)
-            resudi.update({ sekor : metakesi })
+            resudi = {} # { score : last keywo-keywo}
+            for metakesi in fudidi.keys():
+                sekor = fudidi.get(metakesi)
+                resudi.update({ sekor : metakesi })
 
-        resut = {}
-        fikesi = resudi.get(max(sorted(list(resudi.keys()))))
-        finkase = fikasi.split('@')
-        finkese = fikesi.split('@')
-        for numelo in range(0,len(finkase)):
-            resut.update({ finkase[numelo] : finkese[numelo] })
+            resut = {}
+            fikesi = resudi.get(max(sorted(list(resudi.keys()))))
+            fikase = fikasi.split('@')
+            fikese = fikesi.split('@')
+            if len(fikase) != len(fikese):
+                print("fikasi: "+fikasi)
+                print("fikesi: "+fikesi)
+                pprint.pprint(fikase)
+                pprint.pprint(fikese)
+            else:
+                for numelo in range(0,len(fikase)):
+                    if fikese[numelo] != '':
+                        resut.update({ fikase[numelo] : fikese[numelo] })
 
-        #return resut
-        pprint.pprint(resut)
+            # pprint.pprint(resut)
+            return resut
 
     def codCreo(self):
         """Condition of general function"""
