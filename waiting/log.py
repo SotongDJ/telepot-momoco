9d72f83dd969fec4221dcca2f75c46fa59c99476
David Soh <sotong0221@gmail.com>
2018-01-31 23:04:04 +0800

    move all reference files to refer and remove them from repo

--- waiting/log.py
+++
@@ -1,8 +0,0 @@
-import tool
-def logging(id,lines,source):
-    nana=open(tool.path("log",usrid=id)+source,"a")
-    nana.write(lines+"\n")
-    nana.close()
-    print(lines)
-
-#def logset(id,sets,location):    lines=""    for n in range(0,len(sets)):        lines=lines+sets[n]        if n == len(sets):            lines=lines+"\n"        else:            lines=lines+","    logging(id,lines,location)
