e14286a4c6ad61a31162cd56383f44a379697243
David Soh <sotong0221@gmail.com>
2018-02-06 17:00:53 +0800

    split out listKeywo()

--- modRecom.py
+++ modRecom.py
@@ -1,23 +1,7 @@
 from core import modDatabase
 from core import tool
 
-def listKeywo(usrdir):
-    print('modDatabase.listKeywo: '+tool.mask(usrdir))
-    keydb = modDatabase.opendb(usrdir).get('key',{})
-    resut = {}
-    for kas in keydb.keys():
-        if kas not in ['datte','price','tpric','desci','karen','tkare']:
-            for keywo in keydb.get(kas,{}).keys():
-                numon = 0
-                metdi = {}
-                metse = []
-
-                metdi = resut.get(keywo,{})
-                metse = metdi.get(kas,[])
-                metse.extend(keydb.get(kas,{}).get(keywo,[]))
-                metdi.update({ kas : metse })
-                resut.update({ keywo : metdi })
-    return resut
+import modKeywo
 
 def exper(usrdir,keywo):
     """Grab word from msg"""
