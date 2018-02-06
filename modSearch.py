a86699c1bb790fa4099a520ee7af970d5a56df5c
David Soh <sotong0221@gmail.com>
2018-02-06 22:40:52 +0800

    finish modifying for Arrangement func. of exper() in modSearch

--- modSearch.py
+++ modSearch.py
@@ -14,8 +14,6 @@ def exper(usrdir,mesag):
     mesalista = mesag.split(' ')
     mesdik = {} # {class : [keyword]}
     udilis = [] # [uuid]
-    udidik = {} # {uuid : score }
-    dikudi = {0:[0]} # {score : uuid}
 
     for keyto in kewulista.keys():
         if keyto in mesag:
@@ -25,80 +23,52 @@ def exper(usrdir,mesag):
                 mesdik.update({ kasse : metase })
                 udilis.extend(kewulista.get(keyto).get(kasse))
     # pprint.pprint(mesdik)
-    nummo = 0
-    for uuid in udilis:
-        nummo = udidik.get(uuid,0)
-        nummo = nummo + 1
-        udidik.update({ uuid : nummo })
-
-    setta = []
-    for uuid in udidik:
-        nummo = udidik.get(uuid)
-        setta = dikudi.get(nummo,[])
-        setta.append(uuid)
-        dikudi.update({ nummo : setta })
-    # pprint.pprint(dikudi)
 
     finudi = [] # [uuid]
     fikali = [] # [class]
     fikeli = [] # [keyword]
     fikasi = '' # class-class...
-    fudidi = {} # { keywo-keywo... : score}
+    fudidi = {} # { keywo-keywo... : [uuid]}
 
     kasse = ''
-    finudi = dikudi.get(max(sorted(list(dikudi.keys()))))
-    if finudi == [0]:
-        return {}
-    else:
-        fikali = sorted(list(mesdik.keys()))
-        # pprint.pprint(fikeli)
-        for kasse in fikali:
-            if fikasi == "":
-                fikasi = kasse
-            else:
-                fikasi = fikasi + '@' + kasse
-        # print("fikasi:"+fikasi)
-        for kewoli in mesdik.values():
-            fikeli.extend(kewoli)
-        nummo = 0
-        for uuid in finudi:
-            metasi = ''
-            for kasse in fikali:
-                keyta = rawdb.get(uuid).get(kasse)
-                # print("keyta:"+keyta)
-                if keyta not in fikeli:
-                    keyta = ''
-                if metasi == '':
-                    if keyta == '':
-                        metasi = '@'
-                    else:
-                        metasi = keyta
-                elif metasi == '@':
-                    metasi = metasi + keyta
-                else:
-                    metasi = metasi + '@' + keyta
-            nummo = fudidi.get(metasi,0)
-            nummo = nummo + 1
-            fudidi.update({ metasi : nummo })
-        # pprint.pprint(fudidi)
-
-        resudi = {} # { score : last keywo-keywo}
-        for metakesi in fudidi.keys():
-            sekor = fudidi.get(metakesi)
-            resudi.update({ sekor : metakesi })
-
-        resut = {}
-        fikesi = resudi.get(max(sorted(list(resudi.keys()))))
-        fikase = fikasi.split('@')
-        fikese = fikesi.split('@')
-        if len(fikase) != len(fikese):
-            print("fikasi: "+fikasi)
-            print("fikesi: "+fikesi)
-            pprint.pprint(fikase)
-            pprint.pprint(fikese)
+    finudi = sorted(list(udilis))
+    fikali = sorted(list(mesdik.keys()))
+    # pprint.pprint(fikali)
+    for kewoli in mesdik.values():
+        fikeli.extend(kewoli)
+    # pprint.pprint(fikeli)
+    for kasse in fikali:
+        if fikasi == "":
+            fikasi = kasse
         else:
-            for numelo in range(0,len(fikase)):
-                if fikese[numelo] != '':
-                    resut.update({ fikase[numelo] : fikese[numelo] })
-        # pprint.pprint(resut)
-        return resut
+            fikasi = fikasi + '@' + kasse
+    # print("fikasi:"+fikasi)
+    nummo = 0
+    for uuid in finudi:
+        metasi = ''
+        for kasse in fikali:
+            keyta = rawdb.get(uuid).get(kasse)
+            # print("keyta:"+keyta)
+            if keyta not in fikeli:
+                keyta = ''
+
+            if metasi == '':
+                if keyta == '':
+                    metasi = '@'
+                else:
+                    metasi = keyta
+            elif metasi == '@':
+                metasi = metasi + keyta
+            else:
+                metasi = metasi + '@' + keyta
+
+        metaso = fudidi.get(metasi,[])
+        metaso.append(uuid)
+        fudidi.update({ metasi : metaso })
+    # pprint.pprint(fudidi)
+
+    resut = {}
+    resut.update({ 'fikasi' : fikasi})
+    resut.update({ 'fudidi' : fudidi})
+
+    return resut
