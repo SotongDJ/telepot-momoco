a2f050e857022e6265e65e3d84a0781c16a4c1e8
David Soh <sotong0221@gmail.com>
2018-02-07 17:07:06 +0800

    add modSearch.numof() for detect date

--- modSearch.py
+++ modSearch.py
@@ -3,9 +3,71 @@ from core import tool
 
 import modKeywo,pprint
 
-def exper(usrdir,mesag,btempo='',ftempo='',preudi=[]):
+def numof(mesag):
+    print('modSearch.numof')
+    print('mesag: '+mesag)
+    pre = False
+    resut = '0'
+    metasi = ''
+    metase = []
+    numan = ['0','1','2','3','4','5','6','7','8','9']
+    for stik in mesag:
+        if stik == '#':
+            if pre:
+                metase.append(metasi)
+                metasi = ''
+            else:
+                pre = True
+        elif stik in numan:
+            if pre:
+                metasi = metasi + stik
+        elif stik == '-':
+            if pre:
+                metasi = metasi + stik
+        else:
+            pre = False
+            if metasi != '':
+                metase.append(metasi)
+                metasi = ''
+    if pre:
+        pre = False
+        if metasi != '':
+            metase.append(metasi)
+            metasi = ''
+
+    # pprint.pprint(metase)
+    metadi = {}
+    for numdo in metase:
+        if len(numdo) > 10:
+            null = metase.pop(metase.index(numdo))
+        elif len(numdo) >= 5:
+            if numdo[4] != '-':
+                null = metase.pop(metase.index(numdo))
+            elif len(numdo) >= 8:
+                if numdo[7] != '-':
+                    null = metase.pop(metase.index(numdo))
+    for numdo in metase:
+        domun = numdo.replace('-','')
+        if len(domun) < 8 :
+            domun = domun + '0'*(8-len(domun))
+        metadi.update({int(domun):numdo})
+    btempo = metadi.get(max(metadi.keys()))
+    ftempo = metadi.get(min(metadi.keys()))
+    resut = {
+        'datese' : metase,
+        'btempo' : btempo,
+        'ftempo' : ftempo,
+    }
+    return resut
+
+
+def exper(usrdir,mesag,preudi=[]):
     """Grab word from msg"""
-    print('modRecom.exper: '+tool.mask(usrdir))
+    print('modSearch.exper: '+tool.mask(usrdir))
+    tempo = numof(mesag)
+    btempo = tempo.get('btempo','')
+    ftempo = tempo.get('ftempo','')
+    datese = tempo.get('datese','')
     if preudi == []:
         preudi = modDatabase.timra(usrdir,btempo=btempo,ftempo=ftempo,modde='uuid')
     rawdb = modDatabase.opendb(usrdir).get('raw',{})
