96f93812e5e3311641fa3d54c619b5cc4f95978c
David Soh <sotong0221@gmail.com>
2018-02-20 23:22:15 +0800

    modulize filta workflow and create filtan(), resan()

--- msgSaci.py
+++ msgSaci.py
@@ -4,23 +4,7 @@ class MsgSaci:
     def __init__(self,lingua='SiMP'):
         self.lingua = lingua
 
-    def setion(self,danno={}):
-        resut = open('descri/saciSetion.'+self.lingua).read()
-        kassi = danno.get('fikasi','').split('@')
-        kessi = danno.get('namnan','').split('@')
-
-        setto = {}
-        n = 0
-        tabol = True
-        while tabol:
-            if kessi[n] != '':
-                setto.update({ kassi[n] : kessi[n] })
-            n = n + 1
-            if n >= len(kassi):
-                tabol = False
-            if n >= len(kessi):
-                tabol = False
-        udise = danno.get('fudidi',[])
+    def filtan(self,setto={}):
         filta = ''
         if 'datte' in setto.keys():
             filta = filta + '。datte: ' + setto.get('datte') + '\n'
@@ -70,9 +54,103 @@ class MsgSaci:
             frasa = frasa.replace('@tpric@','　　')
         if tobol:
             filta = filta + frasa
+        # print(filta)
+        return filta
 
-        pprint.pprint(setto)
-        print(filta)
+    def resan(self,udidi={}):
+        filta = '　'
+        if 'datte' in udidi.keys():
+            filta = filta + udidi.get('datte') + '\n'
+
+        nabol = False
+        frasa = '　'
+        if 'namma' in udidi.keys():
+            frasa = frasa + udidi.get('namma') + '　'
+            nabol = True
+        if 'klass' in udidi.keys():
+            frasa = frasa + udidi.get('klass') + '　'
+            nabol = True
+        if 'shoop' in udidi.keys():
+            frasa = frasa + udidi.get('shoop') + '　'
+            nabol = True
+        if nabol:
+            filta = filta + frasa + '\n'
+
+
+        fobol = False
+        frasa = '　F: [@fromm@] @karen@ @price@\n'
+        if 'fromm' in udidi.keys():
+            frasa = frasa.replace('@fromm@',udidi.get('fromm'))
+            fobol = True
+        else:
+            frasa = frasa.replace('@fromm@','　　')
+        if 'karen' in udidi.keys():
+            frasa = frasa.replace('@karen@',udidi.get('karen'))
+            fobol = True
+        else:
+            frasa = frasa.replace('@karen@','　　')
+        if 'price' in udidi.keys():
+            frasa = frasa.replace('@price@',udidi.get('price'))
+            fobol = True
+        else:
+            frasa = frasa.replace('@price@','　　')
+        if fobol:
+            filta = filta + frasa
+
+        tobol = False
+        frasa = '　T: [@toooo@] @tkare@ @tpric@\n'
+        if 'toooo' in udidi.keys():
+            frasa = frasa.replace('@toooo@',udidi.get('toooo'))
+            tobol = True
+        else:
+            frasa = frasa.replace('@toooo@','　　')
+        if 'tkare' in udidi.keys():
+            frasa = frasa.replace('@tkare@',udidi.get('tkare'))
+            tobol = True
+        else:
+            frasa = frasa.replace('@tkare@','　　')
+        if 'tpric' in udidi.keys():
+            frasa = frasa.replace('@tpric@',udidi.get('tpric'))
+            tobol = True
+        else:
+            frasa = frasa.replace('@tpric@','　　')
+        if tobol:
+            filta = filta + frasa
+        # print(filta)
+        return filta
+
+    def setion(self,danno={},rawdi={}):
+        resut = open('descri/saciSetion.'+self.lingua).read()
+        kassi = danno.get('fikasi','').split('@')
+        kessi = danno.get('namnan','').split('@')
+
+        setto = {}
+        n = 0
+        tabol = True
+        while tabol:
+            if kessi[n] != '':
+                setto.update({ kassi[n] : kessi[n] })
+            n = n + 1
+            if n >= len(kassi):
+                tabol = False
+            if n >= len(kessi):
+                tabol = False
+        # pprint.pprint(setto)
+
+        filta = self.filtan(setto=setto)
+
+        udise = danno.get('fudidi',[])
+        resal = ''
+        for uuid in udise:
+            udidi = rawdi.get(uuid,{})
+            for kassi in list(setto.keys()):
+                if kassi not in ['fromm','karen','price','toooo','tkare','tpric']:
+                    if setto.get(kassi,'-') == udidi.get(kassi,'='):
+                        null = udidi.pop(kassi)
+            resal = resal + "/uuid" + uuid + "\n"
+            resal = resal + self.resan(udidi=udidi)
+
         resut = resut.replace('@filta@',filta)
+        resut = resut.replace('@resal@',resal)
 
         return resut
